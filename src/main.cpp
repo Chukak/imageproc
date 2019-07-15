15585c41c13988ad6c560ee283f2bdce7d8be106
Chukak <chukak12@gmail.com>
2019-07-15 19:59:59 +0300

    Updated main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
+#include "settings/arguments.h"
+#include "execute/wrapper.h"
+#include <opencv2/highgui.hpp>
 #include <iostream>
 
-using namespace std;
+using namespace command_line;
 
-int main()
+int main(int argc, char** argv)
 {
-    cout << "Hello World!" << endl;
+    Wrapper w{nullptr};
+    Arguments args(argc, argv, &w);
+
+    if (!args.is_valid()) {
+         std::cerr << "not valid\n";
+        return 1;
+    }
+
+    if (args.is_finish()) {
+        std::cerr << "finish\n";
+        return 0;
+    }
+
+    if (!w.run_exec()) {
+         std::cerr << "false exec\n";
+        return 1;
+    }
+
+    // todo
+    cv::imshow("output", w.get_result());
+    cv::waitKey();
+
     return 0;
 }
