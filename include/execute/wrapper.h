121293560d97a9cc3c953133abce44cb208da5e2
Chukak <chukak12@gmail.com>
2019-07-15 19:57:25 +0300

    Wrapper for processes

---
+++ include/execute/wrapper.h
@@ -0,0 +1,44 @@
+#ifndef WRAPPER_H
+#define WRAPPER_H
+
+#include "execute/internal/baseprocess.h"
+#include <opencv2/core/mat.hpp>
+
+inline auto make_wrapper() noexcept
+{
+
+}
+
+class Wrapper
+{
+public:
+    Wrapper() = default;
+    Wrapper(BaseProcess *p);
+    ~Wrapper();
+    Wrapper(Wrapper&&) = delete;
+    Wrapper(const Wrapper&) = delete;
+
+    Wrapper& operator=(Wrapper&&) = delete;
+    Wrapper& operator=(const Wrapper&) = delete;
+
+    void set(BaseProcess *p) noexcept;
+    bool run_exec() noexcept;
+    inline auto get_result() const noexcept { return result; }
+
+private:
+    template<typename P>
+    bool run_as_default(P p) noexcept
+    {
+        if (p) {
+            p->exec();
+            result = p->obj()->result;
+            return p->obj()->good();
+        }
+        return false;
+    }
+private:
+    BaseProcess *process{nullptr};
+    cv::Mat result{};
+};
+
+#endif // WRAPPER_H
---
+++ src/execute/wrapper.cpp
@@ -0,0 +1,38 @@
+#include "execute/wrapper.h"
+#include "execute/process.h"
+#include "base/blur.h"
+#include "base/show.h"
+
+Wrapper::Wrapper(BaseProcess *p) :
+    process(p)
+{
+}
+
+Wrapper::~Wrapper()
+{
+    // todo
+    delete process;
+}
+
+void Wrapper::set(BaseProcess *p) noexcept
+{
+    process = p;
+}
+
+bool Wrapper::run_exec() noexcept
+{
+    switch (process->type()) {
+    case BaseProcess::Blur_t: {
+        auto p = dynamic_cast<Process<Blur> *>(process);
+        return run_as_default(p);
+    }
+    case BaseProcess::Show_t: {
+        auto p = dynamic_cast<Process<Show> *>(process);
+        return run_as_default(p);
+    }
+    default:
+        return false;
+    }
+    return false;
+}
+
