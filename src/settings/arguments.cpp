e6dae1f7731472bbbd27597b969b9521dbaa816b
Chukak <chukak12@gmail.com>
2019-07-18 21:13:06 +0300

    Parse arguments for thresholding image processing

--- src/settings/arguments.cpp
+++ src/settings/arguments.cpp
@@ -5,6 +5,7 @@
 #include "execute/wrapper.h"
 #include "base/blur.h"
 #include "base/show.h"
+#include "base/threshold.h"
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -26,7 +27,7 @@ Arguments::Arguments(int argc, char** argv, Wrapper *w)
                 finish = true;
                 return;
             }
-
+            // blur
             if (!strcmp(argv[c], BLUR_OPTION)) {
                 BLUR_TYPE type(BLUR_ORIGINAL);
                 int x = -1, y = -1;
@@ -42,11 +43,12 @@ Arguments::Arguments(int argc, char** argv, Wrapper *w)
                         return;
                     }
                     if (!strcmp(argv[c], blur_opt::BLUR_TYPE)) {
-                        if (argc - c > 1) {
+                        if (argc - c++ > 1) {
                             // todo
-                            if (find_if(begin(blur_opt::BLUR_TYPES),
-                                     end(blur_opt::BLUR_TYPES),
-                                     [&argv, &c](const char * a) -> bool { return !strcmp(a, argv[++c]); }) != end(blur_opt::BLUR_TYPES)) {
+                            if (find_if(begin(blur_opt::BLUR_TYPE_VALUES),
+                                        end(blur_opt::BLUR_TYPE_VALUES),
+                                        [argv, c](const char * a) -> bool { return !strcmp(a, argv[c]); })
+                                    != end(blur_opt::BLUR_TYPE_VALUES)) {
                                 type = Blur::parse_type(argv[c]);
                             } else {
                                 printe(blur::BLUR_TYPE_MSG);
@@ -60,6 +62,11 @@ Arguments::Arguments(int argc, char** argv, Wrapper *w)
                         return;
                     }
                     break;
+                }
+                if (!(argc - c > 0)) {
+                    printe(IMG_NOT_FOUND_MSG);
+                    finish = true;
+                    return;
                 }
                 cv::Mat frame = cv::imread(argv[c]);
                 if (frame.empty()) {
@@ -75,6 +82,68 @@ Arguments::Arguments(int argc, char** argv, Wrapper *w)
                 w->set(process);
                 return;
             }
+
+            // threshold
+            if (!strcmp(argv[c], THRESHOLD_OPTION)) {
+                THRESHOLD_TYPE type(THRESHOLD_BINARY);
+                int16_t value = -1;
+                bool is_gray = false;
+                while (++c < argc) {
+                    if (!strcmp(argv[c], threshold_opt::THRESHOLD_VALUE)) {
+                        if (argc - c > 1) {
+                            value = atoi(argv[++c]);
+                            if (!Threshold::precheck_value(value)) {
+                                printe(threshold::THRESHOLD_VALUE_MSG);
+                                finish = true;
+                                return;
+                            }
+                            continue;
+                        }
+                        printe(threshold::THRESHOLD_VALUE_MSG);
+                        finish = true;
+                        return;
+                    }
+                    if (!strcmp(argv[c], threshold_opt::THRESHOLD_TYPE)) {
+                        if (argc - c++ > 1) {
+                            if (find_if(begin(threshold_opt::THRESHOLD_TYPE_VALUES),
+                                        end(threshold_opt::THRESHOLD_TYPE_VALUES),
+                                        [argv, c](const char * a) -> bool { return !strcmp(a, argv[c]); })
+                                    != end(threshold_opt::THRESHOLD_TYPE_VALUES)) {
+                                type = Threshold::parse_type(argv[c]);
+                            } else  {
+                                printe(threshold::THRESHOLD_TYPE_MSG);
+                                finish = true;
+                                return;
+                            }
+                            continue;
+                        }
+                        printe(threshold::SET_THRESHOLD_TYPE_MSG);
+                        finish = true;
+                        return;
+                    }
+                    if (!strcmp(argv[c], threshold_opt::THRESHOLD_GRAY)) {
+                        is_gray = true;
+                        continue;
+                    }
+                    break;
+                }
+                if (!(argc - c > 0)) {
+                    printe(IMG_NOT_FOUND_MSG);
+                    finish = true;
+                    return;
+                }
+                cv::Mat frame = cv::imread(argv[c]);
+                if (frame.empty()) {
+                    printe(IMG_NOT_FOUND_MSG);
+                    finish = true;
+                    return;
+                }
+                auto threshold = new Threshold(type, move(frame), value, is_gray);
+                auto process = make_process(threshold, &Threshold::run);
+                w->set(process);
+                return;
+            }
+
             // only image, just show it
             auto show = new Show(argv[c]);
             auto process = make_process(show, &Show::run);
