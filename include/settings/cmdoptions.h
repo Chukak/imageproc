6af1fd9adc84abc0d2910094098dd1b17701bb76
Chukak <chukak12@gmail.com>
2019-07-18 21:12:05 +0300

    Treshold options. Replace BLUR_TYPES with BLUR_TYPE_VALUES.

--- include/settings/cmdoptions.h
+++ include/settings/cmdoptions.h
@@ -8,17 +8,28 @@ namespace options
 
 constexpr const char * HELP_OPTION("--help");
 constexpr const char * BLUR_OPTION("--blur");
+constexpr const char * THRESHOLD_OPTION("--threshold");
 constexpr const char * SAVE_OPTION("--save");
 
 namespace blur_opt
 {
-
 constexpr const char * BLUR_TYPE("--type");
 constexpr const char * BLUR_POINT("--point-anchor");
-constexpr const char * BLUR_TYPES[] = {"gaussian",
-                                       "median",
-                                       "bilaterial"};
+constexpr const char * BLUR_TYPE_VALUES[] = {"gaussian",
+                                             "median",
+                                             "bilaterial"};
+}
 
+namespace  threshold_opt
+{
+constexpr const char * THRESHOLD_TYPE("--type");
+constexpr const char * THRESHOLD_VALUE("--value");
+constexpr const char * THRESHOLD_GRAY("--gray");
+constexpr const char * THRESHOLD_TYPE_VALUES[] = {"binary",
+                                                  "binary-inv",
+                                                  "truncated",
+                                                  "to-zero",
+                                                  "to-zero-inv"};
 }
 
 }
