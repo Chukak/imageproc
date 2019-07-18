c36296147426ba36658f4a7bcfff6c1b5b25510f
Chukak <chukak12@gmail.com>
2019-07-18 21:10:01 +0300

    Case for the Threshold type

--- src/execute/wrapper.cpp
+++ src/execute/wrapper.cpp
@@ -2,6 +2,7 @@
 #include "execute/process.h"
 #include "base/blur.h"
 #include "base/show.h"
+#include "base/threshold.h"
 
 Wrapper::Wrapper(BaseProcess *p) :
     process(p)
@@ -28,6 +29,10 @@ bool Wrapper::run_exec() noexcept
     }
     case BaseProcess::Show_t: {
         auto p = dynamic_cast<Process<Show> *>(process);
+        return run_as_default(p);
+    }
+    case BaseProcess::Threshold_t: {
+        auto p = dynamic_cast<Process<Threshold> *>(process);
         return run_as_default(p);
     }
     default:
