d6bb3c8610db96391c26dd73fd2e149c495fb5a9
Chukak <chukak12@gmail.com>
2019-07-28 22:17:37 +0300

    Parse arguments for filtering image processing

--- src/settings/arguments.cpp
+++ src/settings/arguments.cpp
@@ -6,6 +6,7 @@
 #include "base/blur.h"
 #include "base/show.h"
 #include "base/threshold.h"
+#include "base/linearfilter.h"
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -143,6 +144,71 @@ Arguments::Arguments(int argc, char** argv, Wrapper *w)
                 w->set(process);
                 return;
             }
+            // linear filter
+            if (!strcmp(argv[c], LINEAR_FILTER_OPTION)) {
+                int8_t kernel_size = 3;
+                int x = -1, y = -1;
+                int delta = 0;
+                while (++c < argc) {
+                    if (!strcmp(argv[c], linear_filter_opt::LINEAR_FILTER_ANCHOR)) {
+                        if (argc - c > 2) {
+                            x = atoi(argv[++c]);
+                            y = atoi(argv[++c]);
+                            continue;
+                        }
+                        printe(linear_filter::SET_X_Y_MSG);
+                        finish = true;
+                        return;
+                    }
+                    if (!strcmp(argv[c], linear_filter_opt::LINEAR_FILTER_KERNEL_SIZE)) {
+                        if (argc - c > 1) {
+                            kernel_size = atoi(argv[++c]);
+                            if (!LinearFilter::precheck_kernel_size(kernel_size)) {
+                                printe(linear_filter::SET_LINEAR_FILTER_KERNEL_SIZE_MSG);
+                                finish = true;
+                                return;
+                            }
+                            continue;
+                        }
+                        printe(linear_filter::LINEAR_FILTER_KERNEL_SIZE_MSG);
+                        finish = true;
+                        return;
+                    }
+                    if (!strcmp(argv[c], linear_filter_opt::LINEAR_FILTER_DELTA)) {
+                        if (argc - c > 1) {
+                            delta = atoi(argv[++c]);
+                            continue;
+                        }
+                        printe(linear_filter::SET_LINEAR_FILTER_DELTA_MSG);
+                        finish = true;
+                        return;
+                    }
+                    break;
+                }
+
+                if (!(argc - c > 0)) {
+                    printe(IMG_NOT_FOUND_MSG);
+                    finish = true;
+                    return;
+                }
+                cv::Mat frame = cv::imread(argv[c]);
+                if (frame.empty()) {
+                    printe(IMG_NOT_FOUND_MSG);
+                    finish = true;
+                    return;
+                }
+                auto filter = new LinearFilter(move(frame), kernel_size);
+                if (x > 0 && y > 0) {
+                    filter->set_anchor(x, y);
+                }
+                if (delta != 0) {
+                    filter->set_delta(delta);
+                }
+                auto process = make_process(filter, &LinearFilter::run);
+                w->set(process);
+                return;
+            }
+            
 
             // only image, just show it
             auto show = new Show(argv[c]);
